bc08702d84ca6cf68261908610e53f57b3e845d9
hatlonely <hatlonely@gmail.com>
2014-03-04 11:07:05 +0800

    格式

--- sy 1-1.cpp
+++ sy 1-1.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main()
 {
 	cout << "Li Zhijun,Hello!" << endl;
+	// 输出endl的时候换行，这样看起来更清楚一点，还有注意空格啊！！
 	cout << "welcome to c++ program!"<< endl << "oh,Bye!";
+	// return 语句和前面的空一行吧
 	return 0;
 }
